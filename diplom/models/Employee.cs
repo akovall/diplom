5bc2ac568eaebc0b7f3bb406cbf80d487f881b44
anko-prog <tornadomaster70@gmail.com>
2025-12-07 16:09:18 +0100

    Add core models

---
+++ diplom/models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diplom.models
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int RoleId { get; set; }
+        public string Position { get; set; }
+        public DateTime HireDate { get; set; }
+        public List<TimeEntry> TimeEntries { get; set; }
+
+    }
+}
---
+++ diplom/models/WorkLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diplom.models
+{
+    internal class WorkLog
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public int TaskItemId { get; set; }
+        public DateTime GeneratedAt { get; set; }
+        public double TotalHours { get; set; }
+        public string Description { get; set; }
+
+    }
+}
