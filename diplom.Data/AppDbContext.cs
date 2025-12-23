61ec7f20d591f23832513c4d29ede66ca8be8c9d
anko-prog <tornadomaster70@gmail.com>
2025-12-23 16:17:23 +0100

    Adding a connection to DB

---
+++ diplom.Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using diplom.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace diplom.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<TaskItem> Tasks { get; set; }
+        public DbSet<TimeEntry> TimeLogs { get; set; }
+
+        public AppDbContext()
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=TimeFlow;Username=postgres;Password=123");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Project>()
+                .HasMany(p => p.Tasks)
+                .WithOne(t => t.Project)
+                .HasForeignKey(t => t.ProjectId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
