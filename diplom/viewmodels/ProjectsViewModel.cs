4634c2c04e4a31140ec01534035b43a79504aa6f
anko-prog <tornadomaster70@gmail.com>
2025-12-26 18:27:01 +0100

    Adding Project page markup

--- diplom/viewmodels/ProjectsViewModel.cs
+++ diplom/viewmodels/ProjectsViewModel.cs
@@ -1,16 +1,65 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 
 namespace diplom.viewmodels
 {
+    public class ProjectDisplayItem : ObservableObject
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+        public int TaskCount { get; set; }
+        public int CompletedTaskCount { get; set; }
+        public double Progress => TaskCount > 0 ? (double)CompletedTaskCount / TaskCount : 0;
+        public string ProgressText => $"{CompletedTaskCount}/{TaskCount} tasks";
+    }
+
     public class ProjectsViewModel : ObservableObject
     {
-        public ProjectsViewModel() { 
-        
+        public ObservableCollection<ProjectDisplayItem> Projects { get; set; }
+        public string SearchQuery { get; set; }
+        public IRelayCommand CreateProjectCommand { get; }
+
+        public ProjectsViewModel()
+        {
+            Projects = new ObservableCollection<ProjectDisplayItem>
+            {
+                new ProjectDisplayItem 
+                { 
+                    Name = "Administration", 
+                    Description = "System administration and infrastructure management tasks.",
+                    Color = "#E53E3E",
+                    TaskCount = 12,
+                    CompletedTaskCount = 8
+                },
+                new ProjectDisplayItem 
+                { 
+                    Name = "Test", 
+                    Description = "Quality assurance and testing automation project.",
+                    Color = "#38A169",
+                    TaskCount = 24,
+                    CompletedTaskCount = 18
+                },
+                new ProjectDisplayItem 
+                { 
+                    Name = "Diplom", 
+                    Description = "Diploma project - task management system development.",
+                    Color = "#3182CE",
+                    TaskCount = 15,
+                    CompletedTaskCount = 6
+                },
+                new ProjectDisplayItem 
+                { 
+                    Name = "Marketing", 
+                    Description = "Marketing campaigns and brand development initiatives.",
+                    Color = "#D69E2E",
+                    TaskCount = 8,
+                    CompletedTaskCount = 3
+                },
+            };
+
+            CreateProjectCommand = new RelayCommand(() => { });
         }
     }
 }
--- diplom/views/ProjectsView.xaml.cs
+++ diplom/views/ProjectsView.xaml.cs
@@ -1,7 +1,27 @@
+using System;
+using System.Globalization;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace diplom.views
 {
+    public class ProgressToWidthConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 2 && values[0] is double progress && values[1] is double totalWidth)
+            {
+                return progress * totalWidth;
+            }
+            return 0;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public partial class ProjectsView : UserControl
     {
         public ProjectsView()
